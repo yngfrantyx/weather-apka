39138c2d38359b08cf603fb78243eb6766482ace
yngfrantyx <frantisek.haba@orbiszlin.cz>
2025-06-04 09:54:46 +0200

    přidání android složky a za chvíli budu dělat na mobil

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
+import {ToastController} from '@ionic/angular';
 
 const API_URL = environment.API_URL;
 const API_KEY = environment.API_KEY;
@@ -12,29 +13,39 @@ const API_KEY = environment.API_KEY;
   standalone: false,
 })
 export class HomePage {
-  weatherTemp: any
-  todayDate = new Date()
-  cityName = ""
-  weatherIcon: any
-  weatherDetails: any
-  name = ""
-  loading = true
+  weatherTemp: any;
+  todayDate = new Date();
+  cityName = "";
+  weatherIcon: any;
+  weatherDetails: any;
+  name = "";
+  loading = true;
 
-  constructor(public httpClient: HttpClient) {
-    // this.loadData()
+  constructor(
+    public httpClient: HttpClient,
+    private toastController: ToastController // ⬅️ ToastController v konstruktoru
+  ) {
   }
 
   loadData() {
     this.httpClient.get<any>(`${API_URL}/weather?q=${this.cityName}&appid=${API_KEY}`).subscribe(results => {
       console.log(results);
-      this.weatherTemp = results['main']
-      this.name = results['name']
+      this.weatherTemp = results['main'];
+      this.name = results['name'];
       console.log(this.weatherTemp);
-      this.weatherDetails = results['weather'][0]
+      this.weatherDetails = results['weather'][0];
       console.log(this.weatherDetails);
-      this.weatherIcon = `https://openweathermap.org/img/wn/${this.weatherDetails.icon}@4x.png`
-      this.loading = false
-    })
-  }
+      this.weatherIcon = `https://openweathermap.org/img/wn/${this.weatherDetails.icon}@4x.png`;
+      this.loading = false;
+    }, error => {
+      console.log(error);
 
+
+      this.toastController.create({
+        message: 'Failed to load data. Check the city name.',
+        duration: 2000,
+        color: 'danger'
+      }).then(toast => toast.present());
+    });
+  }
 }
